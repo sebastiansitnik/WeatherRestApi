775f96301cb49e9b1779badfdc948d5227811b55
ssit <2sitto@gmail.com>
2020-01-12 12:48:28 +0100

    First commit

---
+++ src/main/java/java11/sda/WeatherRestApi/WeatherRestApiApplication.java
@@ -0,0 +1,13 @@
+package java11.sda.WeatherRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java11/sda/WeatherRestApi/WeatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package java11.sda.WeatherRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
